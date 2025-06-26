6994a0b71eb09e477d021e0b1942a127dbc01892
jan_pfaender <janlukas.pfaender@gmail.com>
2025-06-26 18:23:08 +0200

    upload preliminary version of Madrid presentation

--- R/plot_theme.R
+++ R/plot_theme.R
@@ -1,6 +1,7 @@
 # set general theme for plots
 plot_theme <- theme_minimal(base_size = 12) +
-  theme(# Bold title
+  theme(
+    # Bold title
     plot.title = element_text(face = "bold", size = rel(0.7)),
     # Plain subtitle that is grey
     plot.subtitle = element_text(face = "plain", size = rel(1), color = "grey70"),
@@ -9,5 +10,7 @@ plot_theme <- theme_minimal(base_size = 12) +
     # Bold axis titles
     axis.title = element_text(face = "bold"), 
     # Bold legend labels
-    legend.text = element_text(face = "bold")
-    )
+    legend.text = element_text(face = "bold"),
+    # Bold and slightly larger facet (strip) titles
+    strip.text = element_text(face = "bold", size = rel(1.1))
+  )
